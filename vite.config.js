77301e383dc978ec40951a76c1ab2a6abf0b1e74
Konstantin299 <Konstantin.rbk@gmail.com>
2025-11-20 15:14:05 +0300

    rework

--- vite.config.js
+++ vite.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3005',
+        target: 'https://constrtodo.ru:3005',
         changeOrigin: true,
         secure: false
       }
